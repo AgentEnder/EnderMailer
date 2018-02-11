bc0ba0b6ef806c9312b0ff54f59cdd2663476a24
Craigory V Coppola <craigorycoppola@gmail.com>
2018-02-10 21:36:36 -0500

    Groups
    EnderMailer now supports groups!

--- emailbot.py
+++ emailbot.py
@@ -14,22 +14,33 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 class Contact:
-	def __init__(self, n, a, g):
+	def __init__(self, n, a):
 		self.name = n #User name
 		self.address = a #User email
-		self.group = g #Group user belongs to
+		
+	def __str__(self):
+		return self.name
 	
 
 class Group:
 	def __init__(self, n, i):
 		self.name = n #Group Name
 		self.include = i #Include group when user sends email to all
+		self.members = []
 	
 	def __eq__(self, other):
 		"""Overrides the default implementation"""
 		if isinstance(self, other.__class__):
 			return self.__dict__ == other.__dict__
+		if isinstance(other, str): #This line is kind-of dangerous, could cause unintended behavior but works well for our use cases.
+			return self.name == other
 		return False
+	
+	def __str__(self):
+		return self.name+", " + str(len(self.members))	 + " members" 
+	
+	def addPerson(self, person):
+		self.members.append(person)
 
 def remove_comments(string):
 		pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|#[^\r\n]*$)"
@@ -51,7 +62,6 @@ def parse_comments(file):
 	f.close()
 	return remove_comments(fcontents)
 	
-
 def parse_config():
 	config_options = {}
 	fcontents = parse_comments("config.txt")
@@ -62,13 +72,13 @@ def parse_config():
 	config_options["contact_file"] = flags[2]
 	config_options["msg_file"] = flags[3]
 	config_options["email_username"] = flags[4]
+	config_options["msg_groups"] = flags[5]
 	print("Config Options:")
 	print("------------------")
 	pprint(config_options)
 	print("\n\n")
 	return config_options
 
-	
 def parse_contacts(file):
 	fcontents = parse_comments(file)
 	flines = fcontents.split("\n")
@@ -81,38 +91,87 @@ def parse_contacts(file):
 	contacts = []
 	groups = []
 	currentGroup = Group("Default", True)
-	groups.append(currentGroup)
 	print(currentGroup.name, ':')
 	
 	for line in flines:
 		if line[:2] == "--":
+			groups.append(currentGroup)
 			if(line[2]=='!'):
 				currentGroup = Group(line[3:], False)
 			else:
 				currentGroup = Group(line[2:], True)
-			groups.append(currentGroup)
 			print(currentGroup.name, ':')
 		else:
 			clist = re.findall(r'"([^"]*)"', line)
 			cname = clist[0]
 			caddress = clist[1]
-			cgroup = currentGroup
-			newContact = Contact(cname,caddress,cgroup)
+			newContact = Contact(cname,caddress)
 			print('\t' + newContact.name + ":" + newContact.address)
 			contacts.append(newContact)
-	
+			currentGroup.addPerson(newContact)
+	groups.append(currentGroup)	
 	print("\n\n")
-	
 	return groups, contacts
-	
+
+def read_message(file):
+	return_subject = ""
+	return_message = ""
+	with open(file, 'r') as f:
+		fcontent = remove_comments(f.read())
+		flines = fcontent.split("\n")
+		atMsgText = False
+		for i, line in enumerate(flines):
+			if(flines[i-1] == "--Subject"):
+				return_subject = line
+			elif(flines[i] == "--MessageText"):
+				atMsgText = True
+			elif(atMsgText):
+				return_message += "\n"
+				return_message += line
+	return return_subject, return_message
 
 config = parse_config() #Load Config Data
 
 if(config["contact_file"] == "prompt"):
 	config["contact_file"] = filedialog.askopenfilename(title = "Select Contacts File",filetypes = (("text files","*.txt"),("all files","*.*")))
 
+if(config["msg_file"] == "prompt"):
+	config["msg_file"] = filedialog.askopenfilename(title = "Select Message File",filetypes = (("text files","*.txt"),("all files","*.*")))
+
 
 groups, contacts = parse_contacts(config["contact_file"])
+for group in groups:
+	print (group)
+
+#Groups tos end message to
+if(config['msg_groups'] == "prompt"):
+	print("\nWhich groups would you like to message?")
+	print("Press enter without typing a group name when you are finished.")
+	config["msg_groups"] = []
+	newGroup = ""
+	while(True): #Do-while loop, break if condition is true at the end of the list.
+		newGroup = input("Add group:")
+		if (newGroup == "" or newGroup == "finished"):
+			break;
+		elif(newGroup == "all"):
+			for g in groups:
+				if g.include:
+					config['msg_groups'].append(g.name)
+			break
+		elif(newGroup in groups):
+			config["msg_groups"].append(newGroup)
+		else:
+			print("Please input a valid group.")
+else:
+	temp_groups = config['msg_groups'].split(",")
+	config['msg_groups'] = []
+	for groupName in temp_groups:
+		if(groupName in groups):
+			config["msg_groups"].append(newGroup)
+		else:
+			raise(ValueError(groupName + " does not exist in current contacts file"))
+
+print(config["msg_groups"])
 
 senderAddress = config["email_username"]
 if(senderAddress == "prompt"): #No user email set
@@ -123,32 +182,21 @@ s = smtplib.SMTP(host = config["email_host"], port = config["email_port"])
 s.starttls()
 s.login(senderAddress, senderPassword)
 
-if(config["msg_file"] == "prompt"):
-	config["msg_file"] = filedialog.askopenfilename(title = "Select Message File",filetypes = (("text files","*.txt"),("all files","*.*")))
 
 
-for person in contacts:
-	msg = MIMEMultipart()
-	text = ""
-	with open(config["msg_file"], 'r') as f:
-		fcontent = remove_comments(f.read())
-		flines = fcontent.split("\n")
-		pprint(flines)
-		atMsgText = False
-		for i, line in enumerate(flines):
-			if(flines[i-1] == "--Subject"):
-				msg['Subject'] = line
-			elif(flines[i] == "--MessageText"):
-				atMsgText = True
-			elif(atMsgText):
-				text += "\n"
-				text += line
-	msg['From'] = senderAddress
-	msg['To'] = person.address
-	msg.attach(MIMEText(text, "plain"))
-	
-	s.send_message(msg)
-	
-	del msg
+subject, message = read_message(config["msg_file"])	
+for group in groups:
+	if(group in config["msg_groups"]):
+		for person in contacts:
+			msg = MIMEMultipart()
+			msg['Subject'] = subject
+			
+			msg['From'] = senderAddress
+			msg['To'] = person.address
+			msg.attach(MIMEText(message, "plain"))
+			
+			s.send_message(msg)
+			
+			del msg
 
 input("Press Enter to Close")
